e3de98443aeabef27a8b678b54be9d492e44673c
maticht <maticht.1999@gmail.com>
2024-02-08 06:33:41 +0100

    cars price

--- carOilForm/front/index.js
+++ carOilForm/front/index.js
@@ -5,62 +5,92 @@ document.addEventListener('DOMContentLoaded', function () {
             this.querySelector('.dropdown-menu').classList.toggle('show');
         });
 
-        dropdown.addEventListener('focusout', function () {
-            this.classList.remove('active');
-            this.querySelector('.dropdown-menu').classList.remove('show');
-        });
-
         dropdown.querySelectorAll('.dropdown-menu li').forEach(function (item) {
-            item.addEventListener('click', function () {
+            item.addEventListener('mousedown', function (event) {
+                event.preventDefault();
                 var parentDropdown = this.closest('.dropdown');
                 parentDropdown.querySelector('span').textContent = this.textContent;
                 parentDropdown.querySelector('input').setAttribute('value', this.id);
+
                 if (parentDropdown.id === 'brandList') {
+                    clearModelDropdown();
+                    clearEngineDropdown();
                     updateModelDropdown(this.id);
+                } else if (parentDropdown.id === 'dataList') {
+                    clearEngineDropdown();
+                    displayAdditionalDetails(this.data, this.id);
+                    updateEngineList(this.data);
+                } else if (parentDropdown.id === 'engineList') {
+                    displayAdditionalDetails(this.data, this.id);
                 }
+
+                // Закрываем выпадающий список после выбора элемента
+                parentDropdown.classList.remove('active');
+                parentDropdown.querySelector('.dropdown-menu').classList.remove('show');
             });
         });
     });
 
+    document.addEventListener('mousedown', function (event) {
+        document.querySelectorAll('.dropdown').forEach(function (dropdown) {
+            if (!dropdown.contains(event.target)) {
+                dropdown.classList.remove('active');
+                dropdown.querySelector('.dropdown-menu').classList.remove('show');
+            }
+        });
+    });
+
     function updateModelDropdown(brandId) {
-        fetch('result.json')
+        clearModelDropdown();
+        clearEngineDropdown();
+        clearAdditionalDetails();
+        clearPriceTable();
+        fetch('result1.json')
             .then(response => response.json())
             .then(data => {
                 const modelList = document.getElementById('dataList');
                 modelList.innerHTML = '';
+
+                const uniqueModels = {};
 
                 data.forEach(carData => {
                     if (carData["Марка"].toLowerCase() === brandId.toLowerCase()) {
                         const models = carData["Данные"];
                         models.forEach(modelData => {
                             const modelLabel = modelData["Модель"];
-                            const modificationLabel = modelData["Модификация"];
+                            const modifications = modelData["Модификации"];
 
-                            const li = document.createElement('li');
-                            li.id = `${modelLabel}-${modificationLabel}`.toLowerCase();
-
-                            const modelP = document.createElement('b');
-                            modelP.textContent = modelLabel;
-
-                            const modificationP = document.createElement('p');
-                            modificationP.textContent = modificationLabel;
-
-                            li.appendChild(modelP);
-                            li.appendChild(modificationP);
-
-                            li.addEventListener('click', function () {
-                                var parentDropdown = this.closest('.dropdown');
-                                parentDropdown.querySelector('span').textContent = `${modelLabel}`;
-
-                                // Display additional details
-                                displayAdditionalDetails(modelData);
-
-                                parentDropdown.querySelector('input').setAttribute('value', this.id);
-                            });
-
-                            modelList.appendChild(li);
+                            if (modifications && modifications.length > 0) {
+                                uniqueModels[modelLabel] = modelData;
+                            }
                         });
                     }
+                });
+
+                Object.values(uniqueModels).forEach(modelData => {
+                    const modelLabel = modelData["Модель"];
+
+                    const li = document.createElement('li');
+                    li.id = `${modelLabel}`.toLowerCase();
+                    li.data = modelData;
+
+                    const modelP = document.createElement('b');
+                    modelP.textContent = modelLabel;
+
+                    li.appendChild(modelP);
+
+                    li.addEventListener('click', function () {
+                        var parentDropdown = this.closest('.dropdown');
+                        parentDropdown.querySelector('span').textContent = modelLabel;
+
+                        clearAdditionalDetails();
+
+                        parentDropdown.querySelector('input').setAttribute('value', this.id);
+
+                        updateEngineList(modelData);
+                    });
+
+                    modelList.appendChild(li);
                 });
 
                 document.querySelector('.dropdown-menu#dataList + input').value = '';
@@ -68,17 +98,113 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.error('Error fetching data:', error));
     }
 
-    function displayAdditionalDetails(modelData) {
-        const additionalDetailsElement = document.getElementById('additionalDetails');
-        additionalDetailsElement.innerHTML = '';
-        for (const [key, value] of Object.entries(modelData)) {
-            const detailP = document.createElement('p');
-            detailP.textContent = `${key}: ${value}`;
-            additionalDetailsElement.appendChild(detailP);
-        }
+    function clearModelDropdown() {
+        const modelDropdown = document.getElementById('dataListBlock');
+        modelDropdown.querySelector('span').textContent = 'Выберите модель';
+        modelDropdown.querySelector('input').setAttribute('value', '');
+        modelDropdown.classList.remove('active');
+        modelDropdown.querySelector('.dropdown-menu').classList.remove('show');
     }
 
-    fetch('result.json')
+    function clearEngineDropdown() {
+        const engineDropdown = document.getElementById('engineListBlock');
+        engineDropdown.querySelector('span').textContent = 'Выберите двигатель';
+        engineDropdown.querySelector('input').setAttribute('value', '');
+        engineDropdown.classList.remove('active');
+        engineDropdown.querySelector('.dropdown-menu').classList.remove('show');
+    }
+
+    function clearPriceTable() {
+        const priceTable = document.getElementById('priceTable');
+        const rows = priceTable.querySelectorAll('tbody tr');
+        rows.forEach(row => {
+            row.cells[2].textContent = '';
+            row.cells[3].textContent = '';
+        });
+    }
+
+    function displayAdditionalDetails(modelData, modelTitle) {
+        const additionalDetailsElement = document.getElementById('additionalDetails');
+        additionalDetailsElement.innerHTML = '';
+
+        if (modelData) {
+            for (const [key, value] of Object.entries(modelData)) {
+                if (key !== "Модификация") {
+                    const detailP = document.createElement('p');
+                    detailP.textContent = `${key}: ${value}`;
+                    additionalDetailsElement.appendChild(detailP);
+
+                    if (key === "Объем масла") {
+                        updateVolumeAndSum(value);
+                    }
+                }
+            }
+        }
+
+    }
+
+    function updateVolumeAndSum(volume) {
+        const priceTable = document.getElementById('priceTable');
+        const rows = priceTable.querySelectorAll('tbody tr');
+
+        rows.forEach(row => {
+            const coefficient = parseFloat(row.getAttribute('data-coefficient'));
+            const calculatedVolume = parseFloat(volume.replace(',', '.')) || 0;
+            const calculatedSum = (calculatedVolume * coefficient) + 123;
+
+            row.cells[2].textContent = calculatedVolume.toFixed(2);
+            row.cells[3].textContent = calculatedSum.toFixed(2);
+        });
+
+        calculateTotal();
+    }
+
+    function calculateTotal() {
+        const priceTable = document.getElementById('priceTable');
+        const rows = priceTable.querySelectorAll('tbody tr');
+
+        let totalSum = 0;
+
+        rows.forEach(row => {
+            totalSum += parseFloat(row.cells[3].textContent);
+        });
+
+        document.getElementById('totalSum').textContent = totalSum.toFixed(2);
+    }
+
+    function updateEngineList(modelData) {
+        clearEngineDropdown();
+        clearAdditionalDetails();
+        clearPriceTable()
+        const engineList = document.getElementById('engineList');
+        engineList.innerHTML = '';
+
+        const modifications = modelData["Модификации"];
+
+        modifications.forEach(modificationData => {
+            const modificationLabel = modificationData["Модификация"];
+
+            const li = document.createElement('li');
+            li.textContent = modificationLabel;
+
+            li.addEventListener('click', function () {
+                var parentDropdown = this.closest('.dropdown');
+                parentDropdown.querySelector('span').textContent = modificationLabel;
+                parentDropdown.querySelector('input').setAttribute('value', modificationLabel);
+
+                displayAdditionalDetails(modificationData);
+            });
+
+            engineList.appendChild(li);
+        });
+    }
+
+    function clearAdditionalDetails() {
+        const additionalDetailsElement = document.getElementById('additionalDetails');
+        additionalDetailsElement.innerHTML = '';
+    }
+
+    fetch('result1.json')
         .then(response => response.json())
         .then(data => {
             const uniqueBrands = {};
@@ -107,6 +233,7 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error fetching data:', error));
 });
+
 
 
 
--- carOilForm/jsonArrayParser.js
+++ carOilForm/jsonArrayParser.js
@@ -10,25 +10,42 @@ data.forEach(carData => {
     if (!groupedData[mark]) {
         groupedData[mark] = [];
     }
+    const existingMarkData = groupedData[mark];
+    const existingModel = existingMarkData.find(item => item["Модель"] === carData["Модель"]);
 
-    groupedData[mark].push({
-        "Модель": carData["Модель"],
-        "Модификация": carData["Модификация"],
-        "Тип Двигателя": carData["Тип Двигателя"],
-        "Мощность": carData["Мощность"],
-        "Тип топлива": carData["Тип топлива"],
-        "Год выпуска": carData["Год выпуска"],
-        "Обьем двигателя": carData["Обьем двигателя"],
-        "Объем масла": carData["Объем масла"]
-    });
+    if (existingModel) {
+        existingModel["Модификации"].push({
+            "Модификация": carData["Модификация"],
+            "Тип Двигателя": carData["Тип Двигателя"],
+            "Мощность": carData["Мощность"],
+            "Тип топлива": carData["Тип топлива"],
+            "Год выпуска": carData["Год выпуска"],
+            "Обьем двигателя": carData["Обьем двигателя"],
+            "Объем масла": carData["Объем масла"]
+        });
+    } else {
+        existingMarkData.push({
+            "Модель": carData["Модель"],
+            "Модификации": [
+                {
+                    "Модификация": carData["Модификация"],
+                    "Тип Двигателя": carData["Тип Двигателя"],
+                    "Мощность": carData["Мощность"],
+                    "Тип топлива": carData["Тип топлива"],
+                    "Год выпуска": carData["Год выпуска"],
+                    "Обьем двигателя": carData["Обьем двигателя"],
+                    "Объем масла": carData["Объем масла"]
+                }
+            ]
+        });
+    }
 });
 
 const resultArray = Object.keys(groupedData).map(mark => ({
     "Марка": mark,
-    "Данные": groupedData[mark]
+    "Модели": groupedData[mark]
 }));
 
 const resultJson = JSON.stringify(resultArray, null, 2);
-console.log(`Количество марок: ${Object.keys(groupedData).length}`);
 
 fs.writeFileSync('result.json', resultJson);
